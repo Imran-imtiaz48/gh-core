0a02792cf32332402c26bf2419da033a4932cb86
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-03-13 21:08:14 +0400

    Update 1 - TestSourceDBCreateTables.sql

--- src/main/resources/DataWarehouseTest/1 - TestSourceDBCreateTables.sql
+++ src/main/resources/DataWarehouseTest/1 - TestSourceDBCreateTables.sql
@@ -1,93 +1,100 @@
-use TestGrinHouse
-	
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Greenhouse]') AND type in (N'U'))
+USE TestGrinHouse;
 
-CREATE TABLE Greenhouse(
-	greenhouseId INT PRIMARY KEY NOT NULL,
-	greenhouseName NCHAR VARYING(100) NOT NULL,
-	loginName NCHAR VARYING(100) NOT NULL,
-	loginPassword NCHAR VARYING(100) NOT NULL
-);
+-- Ensure Greenhouse table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Greenhouse]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.Greenhouse (
+        greenhouseId INT PRIMARY KEY NOT NULL,
+        greenhouseName NVARCHAR(100) NOT NULL,
+        loginName NVARCHAR(100) NOT NULL,
+        loginPassword NVARCHAR(100) NOT NULL
+    );
+END;
 
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[HumidifierState]') AND type in (N'U'))
+-- Ensure HumidifierState table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[HumidifierState]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.HumidifierState (
+        humidifierId INT IDENTITY PRIMARY KEY NOT NULL,
+        isHumidifierOn BIT NOT NULL,
+        isDeHumidifierOn BIT NOT NULL,
+        stateDateTime DATETIME NOT NULL,
+        greenhouseId INT NOT NULL,
+        CONSTRAINT FK_HumidifierState_Greenhouse FOREIGN KEY (greenhouseId) REFERENCES dbo.Greenhouse(greenhouseId)
+    );
+END;
 
-CREATE TABLE HumidifierState(
-	humidifierId INT IDENTITY PRIMARY KEY NOT NULL,
-	isHumidifierOn BIT NOT NULL,
-	isDeHumidifierOn BIT NOT NULL,
-	stateDateTime DATETIME NOT NULL,
-	logs INT NOT NULL,
-	FOREIGN KEY(logs) REFERENCES Greenhouse(greenhouseId)
-);
+-- Ensure ACState table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ACState]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.ACState (
+        acStateId INT IDENTITY PRIMARY KEY NOT NULL,
+        isHeaterOn BIT NOT NULL,
+        isCoolerOn BIT NOT NULL,
+        stateDateTime DATETIME NOT NULL,
+        greenhouseId INT NOT NULL,
+        CONSTRAINT FK_ACState_Greenhouse FOREIGN KEY (greenhouseId) REFERENCES dbo.Greenhouse(greenhouseId)
+    );
+END;
 
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[ACSTate]') AND type in (N'U'))
+-- Ensure CarbonDioxideGeneratorState table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[CarbonDioxideGeneratorState]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.CarbonDioxideGeneratorState (
+        carbonDioxideGeneratorId INT IDENTITY PRIMARY KEY NOT NULL,
+        isCarbonDioxideGeneratorOn BIT NOT NULL,
+        stateDateTime DATETIME NOT NULL,
+        greenhouseId INT NOT NULL,
+        CONSTRAINT FK_CarbonDioxideGeneratorState_Greenhouse FOREIGN KEY (greenhouseId) REFERENCES dbo.Greenhouse(greenhouseId)
+    );
+END;
 
-CREATE TABLE ACState(
-	acStateId INT IDENTITY PRIMARY KEY NOT NULL,
-	isHeaterOn BIT NOT NULL,
-	isCoolerOn BIT NOT NULL,
-	stateDateTime DATETIME NOT NULL,
-	logs INT NOT NULL,
-	FOREIGN KEY(logs) REFERENCES Greenhouse(greenhouseId)
-);
+-- Ensure ThresholdProfile table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ThresholdProfile]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.ThresholdProfile (
+        thresholdProfileId INT IDENTITY PRIMARY KEY NOT NULL,
+        profileName NVARCHAR(100) NOT NULL,
+        active BIT NOT NULL,
+        minimumTemperature FLOAT NOT NULL,
+        maximumTemperature FLOAT NOT NULL,
+        minimumHumidity FLOAT NOT NULL,
+        maximumHumidity FLOAT NOT NULL,
+        minimumCarbonDioxide FLOAT NOT NULL,
+        maximumCarbonDioxide FLOAT NOT NULL,
+        greenhouseId INT NOT NULL,
+        CONSTRAINT FK_ThresholdProfile_Greenhouse FOREIGN KEY (greenhouseId) REFERENCES dbo.Greenhouse(greenhouseId)
+    );
+END;
 
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[CarbonDioxideGeneratorState]') AND type in (N'U'))
+-- Ensure MeasurementType table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MeasurementType]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.MeasurementType (
+        title NVARCHAR(20) PRIMARY KEY NOT NULL CHECK (title IN ('temperature', 'humidity', 'carbonDioxide'))
+    );
+END;
 
-CREATE TABLE CarbonDioxideGeneratorState(
-	carbonDioxideGeneratorId INT IDENTITY PRIMARY KEY NOT NULL,
-	isCarbonDioxideGeneratorOn BIT NOT NULL,
-	stateDateTime DATETIME NOT NULL,
-	logs INT NOT NULL,
-	FOREIGN KEY(logs) REFERENCES Greenhouse(greenhouseId)
-);
+-- Ensure Measurement table exists
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Measurement]') AND type = N'U')
+BEGIN
+    CREATE TABLE dbo.Measurement (
+        measurementId INT IDENTITY PRIMARY KEY NOT NULL,
+        measuredValue FLOAT NOT NULL,
+        measurementDateTime DATETIME NOT NULL,
+        greenhouseId INT NOT NULL,
+        measurementType NVARCHAR(20) NOT NULL,
+        CONSTRAINT FK_Measurement_Greenhouse FOREIGN KEY (greenhouseId) REFERENCES dbo.Greenhouse(greenhouseId),
+        CONSTRAINT FK_Measurement_Type FOREIGN KEY (measurementType) REFERENCES dbo.MeasurementType(title)
+    );
+END;
 
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[ThresholdProfile]') AND type in (N'U'))
+-- Insert default values into MeasurementType
+IF NOT EXISTS (SELECT * FROM dbo.MeasurementType WHERE title = 'temperature')
+    INSERT INTO dbo.MeasurementType (title) VALUES ('temperature');
 
-CREATE TABLE ThresholdProfile(
-	thresholdProfileId INT  IDENTITY PRIMARY KEY NOT NULL,
-	profileName NCHAR VARYING(100) NOT NULL,
-	active BIT NOT NULL,
-	minimumTemperature FLOAT NOT NULL,
-	maximumTemperature FLOAT NOT NULL,
-	minimumHumidity FLOAT NOT NULL,
-	maximumHumidity FLOAT NOT NULL,
-	minimumCarbonDioxide FLOAT NOT NULL,
-	maximumCarbonDioxide FLOAT NOT NULL,
-	storedIn INT NOT NULL,
-	FOREIGN KEY(storedIn) REFERENCES Greenhouse(greenhouseId)
-);
+IF NOT EXISTS (SELECT * FROM dbo.MeasurementType WHERE title = 'humidity')
+    INSERT INTO dbo.MeasurementType (title) VALUES ('humidity');
 
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[MeasurementType]') AND type in (N'U'))
-
-
-CREATE TABLE MeasurementType(
-	title NCHAR VARYING(20) PRIMARY KEY NOT NULL CHECK(title IN ('temperature', 'humidity', 'carbonDioxide'))
-);
-
-
-IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Measurement]') AND type in (N'U'))
-
-CREATE TABLE Measurement(
-	measurementId INT IDENTITY PRIMARY KEY NOT NULL,
-	measuredValue FLOAT NOT NULL,
-	measurementDateTime DATETIME NOT NULL,
-	belongsTo INT NOT NULL,
-	isOfType NCHAR VARYING(20),
-	FOREIGN KEY(belongsTo) REFERENCES Greenhouse(greenhouseId),
-	FOREIGN KEY(isOfType) REFERENCES MeasurementType(title)
-);
-
-
-INSERT INTO [TestGrinHouse].[dbo].[MeasurementType] values ('temperature');
-INSERT INTO [TestGrinHouse].[dbo].[MeasurementType] values ('humidity');
-INSERT INTO [TestGrinHouse].[dbo].[MeasurementType] values ('carbonDioxide');
-
-/*
-INSERT INTO [GrinHouse].[dbo].[HumidifierState] values (1, 1, 0,  '2021-03-01 23:26:30.300', 1);
-INSERT INTO [GrinHouse].[dbo].[ACState] values (1, 1, 0,  '2021-03-01 23:26:30.300', 1);
-INSERT INTO [GrinHouse].[dbo].[CarbonDioxideGeneratorState] values (1, 0,  '2021-03-01 23:26:30.300', 1);
-
-INSERT INTO [GrinHouse].[dbo].[Measurement] values ('25.0', '2021-03-01 23:26:30.300', 1, 'temperature');
-INSERT INTO [GrinHouse].[dbo].[Measurement] values ('35.0', '2021-03-01 23:26:30.300', 1, 'humidity');
-INSERT INTO [GrinHouse].[dbo].[Measurement] values ('45.0', '2021-03-01 23:26:30.300', 1, 'carbonDioxide');
-*/
+IF NOT EXISTS (SELECT * FROM dbo.MeasurementType WHERE title = 'carbonDioxide')
+    INSERT INTO dbo.MeasurementType (title) VALUES ('carbonDioxide');
